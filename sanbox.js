f6dbe29c9ac35281e017fd1b54066bef315c9720
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2022-06-20 17:58:48 +0100

    initial commit

---
+++ sanbox.js
@@ -0,0 +1,6 @@
+const names = "Ochiche Juicio Wisdom";
+const height = "6ft 3in";
+const country = "Nigeria";
+
+const print_out = `Name : ${names}\nHeight: ${height}\nCountry: ${country}`;
+console.log(print_out);
